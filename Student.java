162dd8b26d0c0c6c41aaca78ad6cfb204dac0527
Shivam Sharma <123795055+shivam-web-80@users.noreply.github.com>
2025-05-14 17:07:12 +0530

    Student.java

--- Student.java
+++ Student.java
@@ -1,1 +1,17 @@
+public class Student {
+    int rollNo;
+    String name;
+    String course;
+
+    public Student(int rollNo, String name, String course) {
+        this.rollNo = rollNo;
+        this.name = name;
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        return "Roll No: " + rollNo + ", Name: " + name + ", Course: " + course;
+    }
+}
 
